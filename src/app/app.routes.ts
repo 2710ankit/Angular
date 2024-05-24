251396343ef75aef8d03e8ef52a4a26d29a2f7bd
Ankit Aggarwal <ankit.aggarwal@industrybuying.com>
2024-05-24 12:57:20 +0530

    routes made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home/home.component').then((m) => m.HomeComponent),
+    children: [
+      {
+        path: 'tasks',
+        loadComponent: () =>
+          import('./components/tasks/tasks.component').then(
+            (m) => m.TasksComponent
+          ),
+      },
+      {
+        path: 'create',
+        loadComponent: () =>
+          import('./components/create-task/create-task.component').then(
+            (m) => m.CreateTaskComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+];
